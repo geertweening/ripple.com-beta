e017ff5d3cdb2745987871a3df288f1d80229707
Vahe Hovhannisyan <vhpoet@gmail.com>
2012-12-17 17:51:01 -0800

    how it works page

--- wordpress/wp-content/themes/ripple-theme/includes/subnav-howitworks.php
+++ wordpress/wp-content/themes/ripple-theme/includes/subnav-howitworks.php
@@ -10,13 +10,10 @@
 <nav id="subnav">
   <div class="wrapper">
        <ul id="subnav-icons">
-	       	<li class="one-target active"><a href="#one-target"></a></li>
-	       	<li class="two-target"><a href="#two-target"></a></li>
-	       	<li class="three-target"><a href="#three-target"></a></li>
-	       	<li class="four-target"><a href="#four-target"></a></li>
-	       	<li class="five-target"><a href="#five-target"></a></li>
-	       	<li class="six-target"><a href="#six-target"></a></li>
-	       	<li class="start"><a href="#">Start Ripple</a></li>
+	       	<li class="technical-summary active"><a href="#technical-summary"></a></li>
+	       	<li class="virtual-currency"><a href="#virtual-currency"></a></li>
+	       	<li class="the-open-platform"><a href="#the-open-platform"></a></li>
+<!--	       	<li class="start"><a href="#">Start Ripple</a></li>-->
        </ul>
    </div>
 </nav>
