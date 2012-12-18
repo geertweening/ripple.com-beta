d1905bf20867e23d8bb832932c55518d29831d34
jed <jed@ripple.com>
2012-12-18 14:19:43 -0800

    .

--- wordpress/wp-content/themes/ripple-theme/template-home.php
+++ wordpress/wp-content/themes/ripple-theme/template-home.php
@@ -4,15 +4,13 @@
 *
 */
 ?>
-
-<?php get_header() ?>
 	
 	<!-- MAIN -->
 	<div id="main">
 		<!-- WRAPPER -->
 		<div class="wrapper cf">
    		
-			<?php the_post() ?>
+		
 		
 			<?php get_template_part('includes/slides') ?>
 		
@@ -21,5 +19,3 @@
 	</div>
 	<!-- ENDS MAIN -->
 
-		
-<?php get_footer() ?>
