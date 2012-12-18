386b3dd17fe69a44b775f39d0d729ac4ca702ab1
Vahe Hovhannisyan <vhpoet@gmail.com>
2012-12-18 12:10:58 -0800

    "start ripple" button at the right on that top bar for howitworks page

--- wordpress/wp-content/themes/ripple-theme/includes/subnav-howitworks.php
+++ wordpress/wp-content/themes/ripple-theme/includes/subnav-howitworks.php
@@ -13,7 +13,7 @@
 	       	<li class="technical-summary active"><a href="#technical-summary"></a></li>
 	       	<li class="virtual-currency"><a href="#virtual-currency"></a></li>
 	       	<li class="the-open-platform"><a href="#the-open-platform"></a></li>
-<!--	       	<li class="start"><a href="#">Start Ripple</a></li>-->
+	       	<li class="start"><a href="#">Start Ripple</a></li>
        </ul>
    </div>
 </nav>
