c9ec9dfeb7221aa642c539fc21297e97b7671550
Vahe Hovhannisyan <vhpoet@gmail.com>
2012-12-19 16:19:43 -0800

    ripple about page new files

---
+++ wordpress/wp-content/themes/ripple-theme/includes/subnav-aboutripple.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+* Display the subnavigation 
+*
+*/
+?>
+
+
+<!-- Subnavigation -->
+<nav id="subnav">
+  <div class="wrapper">
+       <ul id="subnav-icons">
+	       	<li class="simple active"><a href="#simple"></a></li>
+	       	<li class="truly-open active"><a href="#truly-open"></a></li>
+	       	<li class="global"><a href="#global"></a></li>
+	       	<li class="freeish"><a href="#freeish"></a></li>
+	       	<li class="start"><a href="#">Start Ripple</a></li>
+       </ul>
+   </div>
+</nav>
+<!-- ENDS Subnavigation -->
+
