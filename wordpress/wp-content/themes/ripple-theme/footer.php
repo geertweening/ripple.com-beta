fe5648aef84fe82e00da6cadf9b2b2ab04756384
jed <jed@ripple.com>
2013-02-06 13:24:41 -0800

    footer

--- wordpress/wp-content/themes/ripple-theme/footer.php
+++ wordpress/wp-content/themes/ripple-theme/footer.php
@@ -1,42 +1,17 @@
-
-		<?php
-		// Dont display on home page
-    if(!is_front_page()): ?>
-      <!-- FOOTER -->
-        <footer>
-
-          <div class="wrapper">
-              <ul class="links">
-                  <li>
-                      <span class="title">About Us</span>
-                      <ul>
-                          <li><a href="#">Individuals</a></li>
-                          <li><a href="#">Bitcoin</a></li>
-                          <li><a href="#">Merchants</a></li>
-                          <li><a href="#">Developers</a></li>
-                      </ul>
-                  </li>
-                  <li>
-                      <span class="title">Resources</span>
-                      <ul>
-                          <li><a href="/about-ripple/">What is ripple</a></li>
-                          <li><a href="/how-ripple-works/">How it works</a></li>
-                          <li><a href="/working-with-ripple/">Ripple advanced</a></li>
-                      </ul>
-                  </li>
-              </ul>
-          </div>
-
-          <?php //get_template_part('includes/widgets-columns') ?>
-
-          <hr />
-
-          <div class="copy">&copy; 2012 Ripple</div>
-
-        </footer>
-      <!-- ENDS FOOTER -->
-    <?php endif ?>
-
+<!-- FOOTER -->
+         <footer>
+           <div class="wrapper">
+               <?php get_template_part('includes/widgets-columns') ?>
+           </div>
+      
+        <!-- BOTTOM -->
+        <div class="bottom two-target">
+          <div class="wrapper"><?php echo stripcslashes(get_option('ansimuz_footer')) ?></div> 
+        </div>
+        <!-- ENDS BOTTOM -->
+         </footer>
+      
+<!-- ENDS FOOTER -->
        <?php wp_footer() ?>
     </body>
 </html>
