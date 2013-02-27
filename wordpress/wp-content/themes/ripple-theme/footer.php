839d785c28b94b9ddfc4c93255096e111740f620
jed <jed@ripple.com>
2013-02-27 13:21:55 -0800

    analytics

--- wordpress/wp-content/themes/ripple-theme/footer.php
+++ wordpress/wp-content/themes/ripple-theme/footer.php
@@ -22,6 +22,18 @@
       <script src="<?php echo get_template_directory_uri() ?>/js/jquery.rs.carousel.js"></script>
       <script src="<?php echo get_template_directory_uri() ?>/js/custom.js"></script>
 
+	<script>
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-35589474-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
 <!-- ENDS FOOTER -->
       <?php wp_footer() ?>
     </body>
