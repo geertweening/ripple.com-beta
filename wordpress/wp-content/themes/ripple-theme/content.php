c61182caa38e6ce8dc06167e4c738245b2d504d8
Winnie Lim <wynlim@gmail.com>
2013-04-04 14:43:21 -0700

    blog: display excerpt on blog page and full post on single page

--- wordpress/wp-content/themes/ripple-theme/content.php
+++ wordpress/wp-content/themes/ripple-theme/content.php
@@ -7,10 +7,15 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="entry-header">
+		<span class="entry-date"><?php the_time('F j, Y'); ?></span>
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 	</div>
 	<div class="entry-summary">
-		<?php the_excerpt(); ?>
+		<?php if (is_single()): ?>
+			<?php the_content(); ?>
+		<?php else: ?>
+			<?php the_excerpt(); ?>
+		<?php endif ?>
 		<a href="<?php the_permalink(); ?>">read more</a>
 	</div><!-- .entry-summary -->
 </article><!-- #post-<?php the_ID(); ?> -->
