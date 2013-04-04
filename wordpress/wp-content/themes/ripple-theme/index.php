408b50f2469ea8df732d64a8de47e49fa2cdea52
Winnie Lim <wynlim@gmail.com>
2013-04-04 11:33:32 -0700

    putting the WP Loop into the blog template

--- wordpress/wp-content/themes/ripple-theme/index.php
+++ wordpress/wp-content/themes/ripple-theme/index.php
@@ -1,2 +1,12 @@
 <?php get_header(); ?>
+	<div id="main">
+	<div class="wrapper">
+		<?php if (have_posts()) : ?>
+			<?php while (have_posts()) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>    
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+		<?php endif; ?>
+		</div>
+	</div>
 <?php get_footer(); ?>
