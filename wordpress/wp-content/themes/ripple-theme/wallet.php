76dd2876b2d8e69135713d62fc5d96f53227f0bf
Winnie Lim <wynlim@gmail.com>
2013-04-08 11:57:14 -0700

    changes to the wallet page for Chris

--- wordpress/wp-content/themes/ripple-theme/wallet.php
+++ wordpress/wp-content/themes/ripple-theme/wallet.php
@@ -19,14 +19,14 @@
 				<div class="entry-content">
 					<?php the_content() ?>
 					<div id="wallet">
-						<h2>New to ripple?</h2>
+						<h2>New to Ripple?</h2>
 						<h3>The Ripple Wallet is still in limited beta, let us email you when it is ready for a sneak preview!</h3>
 						<form action="http://opencoin.us4.list-manage.com/subscribe/post?u=245dbc1c47849f034390dc5bf&amp;id=706ebe5aff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
             				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
             				<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
           				</form>
 
-          				<p>Existing users, access your <a href="http://ripple.com/client">wallet here</a>.</p>
+          				<h4 class="existing-call">Existing users, access your <a href="http://ripple.com/client">wallet here</a>.</h4>
 					</div>
 				</div>
 			</div>
