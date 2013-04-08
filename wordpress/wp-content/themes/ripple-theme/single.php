e9304bf02b4ca302e4d8f1671c6928e91d07c13f
Winnie Lim <wynlim@gmail.com>
2013-04-08 11:50:36 -0700

    changing case of ripple blog

--- wordpress/wp-content/themes/ripple-theme/single.php
+++ wordpress/wp-content/themes/ripple-theme/single.php
@@ -2,7 +2,7 @@
 	<div id="main">
 		<div class="wrapper">
 			<!-- start blog posts column -->
-			<h3>the ripple blog</h3>
+			<h3>the Ripple blog</h3>
 			<div class="blog-posts">
 				<?php if (have_posts()) : ?>
 					<?php while ( have_posts() ) : the_post(); ?>
