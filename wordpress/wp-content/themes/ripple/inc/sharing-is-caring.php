7f4b58933931ad4a5543cc153aa863c0f8788cb2
Michael Chavez <bigbassroller@gmail.com>
2013-10-01 11:45:20 -0700

    Re worked "sharing is caring" to be more awesome and less janky. Added plugin that automagically adds schemaorg to website to improve SEO page results.

--- wordpress/wp-content/themes/ripple/inc/sharing-is-caring.php
+++ wordpress/wp-content/themes/ripple/inc/sharing-is-caring.php
@@ -1,42 +1,31 @@
-<div class="table-responsive sharing-is-caring">
-		        <table class="table">
-		          	<tbody>
-			            <tr>
-			            	<td>
-			            		<div class="facebooker pull-left">
-								<fb:like href="https://ripple.com/" width="450" layout="button_count" show_faces="true" send="false"></fb:like>
-								</div>
-							</td>
-			                <td>
-			                	<div class="twitterer pull-left">
-									<a href="https://twitter.com/share" class="twitter-share-button" data-via="bigbassroller">Tweet</a>
-									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-									</script>
-								</div>
-			              	</td>
-			                <td>
-			                	<div class="linkediner pull-left">
-									<script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
-									<script type="IN/Share" data-counter="right"></script>
-								</div>
-			                </td>
-			                <td>
-			              		<div class="gpluser pull-left">
-								<g:plus action="share"></g:plus>
-							    <script type="text/javascript">
-							      window.___gcfg = {
-							        lang: 'en-US'
-							      };
+<ul class="list-unstyled sharing-is-caring">
+   <li class="facebooker">
+		<fb:like href="https://ripple.com/"  layout="button_count" show_faces="true" send="false"></fb:like>
+	</li>
+	<li class="twitterer">
+			<a href="https://twitter.com/share" class="twitter-share-button" data-via="bigbassroller">Tweet</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+			</script>
+	</li>
 
-							      (function() {
-							        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-							        po.src = 'https://apis.google.com/js/plusone.js';
-							        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-							      })();
-							    </script>
-								</div>
-			              	</td>
-			            </tr>
-		          	</tbody>
-		        </table>
-		    </div><!-- /.table-responsive -->
+	<div class="mobile-linebreak"><br></div>
+
+	<li class="linkediner">
+			<script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
+			<script type="IN/Share" data-counter="right"></script>
+    </li>
+    <li class="gpluser">
+		<g:plus action="share"></g:plus>
+	    <script type="text/javascript">
+	      window.___gcfg = {
+	        lang: 'en-US'
+	      };
+
+	      (function() {
+	        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	        po.src = 'https://apis.google.com/js/plusone.js';
+	        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	      })();
+	    </script>
+    </li>
+</ul>
