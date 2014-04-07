d7d38779c84090dda2374e61be7e0f6657c225c4
Michael Chavez <bigbassroller@gmail.com>
2014-04-07 13:30:36 -0700

    Added banner for devloper portal on dev page, touched up some headings to be darker and bolder on blog page

--- wordpress/wp-content/themes/ripple/sidebar-start-using-dev.php
+++ wordpress/wp-content/themes/ripple/sidebar-start-using-dev.php
@@ -5,35 +5,49 @@
  * @packageRipple
  */
 ?>
-	<div id="tertiary" class="tab-pane" role="complementary">
-		<section>
-			<div class="wrapper">
-				<div class="col-lg-12 centered text-center">
-					<h3>Get Started</h3>
-					<p>The Ripple repository is now open source and available at Github.</p>
-					<img alt="Go to Ripple Gitub" src="<?php bloginfo('template_url'); ?>/assets/img/github.png">
-					<div class="row">
-					<a href="https://github.com/ripple" target="blank"><button type="button" class="btn btn-primary">Go to Github</button></a>
-					</div>
-				</div>
-			</div><!-- .wrapper-->
-		</section>
-	</div><!-- #tertiary -->
-
-	<div id="quaternary" class="widget-area shaded" role="complementary">
-		<section>
-			<div class="wrapper">
-				<div class="col-lg-12 centered text-center  shaded">
-					<h3>Find out more at the Ripple Wiki</h3>
-					<p>Learn more about how Ripple works at the Ripple Wiki.</p>
-					<img alt="Ripple Wiki" src="<?php bloginfo('template_url'); ?>/assets/img/wiki.png">
-					<div class="row">
-					<a href="https://ripple.com/wiki/" target="blank"><button type="button" class="btn btn-primary">Go to Wiki</button></a>
-					</div>
+<div class="tab-pane" role="complementary">
+	<section>
+		<div class="wrapper">
+			<div class="col-lg-12 centered text-center">
+				<h3>Developer tools</h3>
+				<p> Check out API documentation and more at the Ripple Developer Portal.</p>
+				<img alt="Go to Ripple Gitub" src="<?php bloginfo('template_url'); ?>/assets/img/black_logo.png">
+				<div class="row">
+					<a href="https://dev.ripple.com/" target="blank"><button type="button" class="btn btn-primary">Go to Developer Portal</button></a>
 				</div>
 			</div>
-		</section>
-	</div>
+		</div><!-- .wrapper-->
+	</section>
+</div>
+<div class="tab-pane" role="complementary">
+	<section>
+		<div class="wrapper">
+			<div class="col-lg-12 centered text-center">
+				<h3>Get Started</h3>
+				<p>The Ripple repository is now open source and available at Github.</p>
+				<img alt="Go to Ripple Gitub" src="<?php bloginfo('template_url'); ?>/assets/img/github.png">
+				<div class="row">
+					<a href="https://github.com/ripple" target="blank"><button type="button" class="btn btn-primary">Go to Github</button></a>
+				</div>
+			</div>
+		</div><!-- .wrapper-->
+	</section>
+</div>
+
+<div class="tab-pane widget-area shaded" role="complementary">
+	<section>
+		<div class="wrapper">
+			<div class="col-lg-12 centered text-center  shaded">
+				<h3>Find out more at the Ripple Wiki</h3>
+				<p>Learn more about how Ripple works at the Ripple Wiki.</p>
+				<img alt="Ripple Wiki" src="<?php bloginfo('template_url'); ?>/assets/img/wiki.png">
+				<div class="row">
+					<a href="https://ripple.com/wiki/" target="blank"><button type="button" class="btn btn-primary">Go to Wiki</button></a>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
 			<!-- <div class="col-lg-8 centered">
 			<h3 class="widget-heading">Start Building Ripple Apps</h3>
 			<div class="col-lg-10 defloat">
@@ -44,39 +58,39 @@
 				<li><strong>Explore the <a href="<?php echo esc_url( home_url( '/' ) ); ?>protocol/">Ripple Transaction Protocol </a> (RTXP).</strong> The protocol that makes Ripple possible.</li>
 			</ul>
 			</div>
-			</div> -->
+		</div> -->
 		
-	<div id="quinary" class="tab-pane widget-area" role="complementary">
-		<section>
-			<div class="wrapper">
-	        		<h3 class="widget-heading text-center">Learn more about Ripple.</h3>
-	        	<div class="col-10 centered">
+<div class="tab-pane widget-area" role="complementary">
+	<section>
+		<div class="wrapper">
+			<h3 class="widget-heading text-center">Learn more about Ripple.</h3>
+			<div class="col-10 centered">
 
-					<div class="col-sm-4">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>payment-network/" class="thumbnail boxed">
-							<p><strong>Learn about the Ripple Payment Network:</strong><br>
-							   A new standard in transactions.
-							</p>
-						</a>
-					</div>
-	 
-					<div class="col-sm-4">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>distributed-fx/" class="thumbnail boxed">
-							<p><strong>Discover Ripple's Distributed Currency Exchange:</strong><br>
-								A universal translator for money.
-							</p>
-						</a>
-					</div>
-	 
-					<div class="col-sm-4">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>protocol/" class="thumbnail boxed">
-							<p><strong>Explore the Ripple Transaction Protocol (RTXP)</strong><br>
-							   The protocol that makes Ripple possible.
-							</p>
-						</a>
-					</div>
-	        	</div>
-	   		</div>
-	   	</section>
-   	</div><!-- quinary -->
+				<div class="col-sm-4">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>payment-network/" class="thumbnail boxed">
+						<p><strong>Learn about the Ripple Payment Network:</strong><br>
+							A new standard in transactions.
+						</p>
+					</a>
+				</div>
+
+				<div class="col-sm-4">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>distributed-fx/" class="thumbnail boxed">
+						<p><strong>Discover Ripple's Distributed Currency Exchange:</strong><br>
+							A universal translator for money.
+						</p>
+					</a>
+				</div>
+
+				<div class="col-sm-4">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>protocol/" class="thumbnail boxed">
+						<p><strong>Explore the Ripple Transaction Protocol (RTXP)</strong><br>
+							The protocol that makes Ripple possible.
+						</p>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
 
