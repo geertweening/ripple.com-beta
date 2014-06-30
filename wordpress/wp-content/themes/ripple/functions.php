7a8685cd15b65e6702cea7c516ad225cb7699a4b
Stefan Thomas <justmoon@members.fsf.org>
2014-06-30 16:15:19 -0700

    [FIX] Added paymentVisualization.js

--- wordpress/wp-content/themes/ripple/functions.php
+++ wordpress/wp-content/themes/ripple/functions.php
@@ -142,6 +142,7 @@ function ripple_scripts() {
 	if (is_page('devs')) {
 		wp_enqueue_script( 'dev-script', get_template_directory_uri() . '/assets/js/dev-script.js', array('jquery'), false, true );	
 	}
+	wp_enqueue_script( 'paymentVisualization', get_template_directory_uri() . '/assets/js/paymentVisualization.js', array('jquery'), false, true);
 	wp_enqueue_script( 'rippleVisualizationSetup', get_template_directory_uri() . '/assets/js/rippleVisualizationSetup.js', array('jquery'), false, true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );}
