f6eeda1a27b1bbd07226f94112be14a5ee2e32b0
sffeds <aabelfedorova@gmail.com>
2014-06-30 12:05:02 -0700

    [FEATURE] Updated Dev Portal Logo on Devs Page
    Created and uploaded new Dev Portal logo for Devs Page
    Styled spacing slightly

--- wordpress/wp-content/themes/ripple/sidebar-start-using-dev.php
+++ wordpress/wp-content/themes/ripple/sidebar-start-using-dev.php
@@ -9,7 +9,7 @@
 	
 	<section>
 		<div class="wrapper dev-info-wrapper">
-			<img class="dev_img" alt="Go to Ripple Gitub" src="<?php bloginfo('template_url'); ?>/assets/img/black_logo.png" />
+			<img class="dev_img" alt="Go to Ripple Gitub" src="https://ripple.com/wp-content/uploads/2014/06/dev_portal_logo_6-30-14.png" />
 			<h3 class="widget-heading secondary-font mm_header">Ripple for Developers</h3>
 			<p class="dev_p">Ripple is an open-source, distributed payments protocol. It enables the instant and free exchange of any store of value - including dollars, euros, yen, 
 			bitcoins, gold, even loyalty points. Businesses of any size can easily build payment solutions, like banking or remittance applications, by tapping into a global payment 
@@ -23,7 +23,7 @@
 				<p> Check out API documentation and more at the Ripple Developer Portal</p>
 				<div class="row">
 					<a href="https://dev.ripple.com/" target="blank"><button type="button" class="btn btn-primary">Go to Developer Portal</button></a>
-			</div>
+				</div>
 		</div><!-- .wrapper-->
 	</section>
 
