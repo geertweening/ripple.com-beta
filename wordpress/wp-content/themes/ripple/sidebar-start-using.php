8886c318556c5ca43a93213f392df566a2644958
Ben Stein-Lobovits <ben@ripple.com>
2014-07-22 10:28:02 -0700

    [TASK] Change Contact Us to mailto links on FI/MM

--- wordpress/wp-content/themes/ripple/sidebar-start-using.php
+++ wordpress/wp-content/themes/ripple/sidebar-start-using.php
@@ -23,25 +23,38 @@
         	<h3 class="tab-pane-heading mm-thumbnail-wrapper">Get in Touch</h3>
         	
         	<div class="wrapper mm-thumbnail-wrapper">
-				<!-- 
-<div class="thumbnail-info">
-					<p>White Paper</p>
-					<div class="row">
-						<a href="https://ripple.com/ripple-mm.pdf" target="blank"><button type="button" class="btn btn-primary">Download</button></a>
-					</div>	
-				</div>
- -->
 
 				<div class="mm_get_in_touch">
 				
 					<div class="row">
-						<a href="https://docs.google.com/a/ripple.com/forms/d/1MbLFFa1_Jt20wc-KUJXKOBnAJq8NX_q5_FJcUIkLfos/viewform?edit_requested=true" target="blank">
-						<button type="button" class="btn btn-primary">Contact Us</button></a>
+						<button type="button" class="btn btn-primary contact_us">
+						<script type="text/javascript" language="javascript">
+							<!--
+							// Email obfuscator script 2.1 by Tim Williams, University of Arizona
+							// Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+							// This code is freeware provided these four comment lines remain intact
+							// A wizard to generate this code is at http://www.jottings.com/obfuscator/
+							{ coded = "eGTN6XTz@T2eeiX.qdF"
+							  key = "VZQMIJuqmSaDWt6OBpf0lbgLhY5F9GwvNs7oeH1irKEAP3kC8xcRzjdX4n2TyU"
+							  shift=coded.length
+							  link=""
+							  for (i=0; i<coded.length; i++) {
+								if (key.indexOf(coded.charAt(i))==-1) {
+								  ltr = coded.charAt(i)
+								  link += (ltr)
+								}
+								else {     
+								  ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+								  link += (key.charAt(ltr))
+								}
+							  }
+							document.write("<a href='mailto:"+link+"?subject=Contact%20Ripple%20Labs%20Business%20Development'>Contact Us</button></a>")
+							}
+							//-->
+							</script><noscript>Sorry, you need Javascript on to email me.</noscript>	
 					</div>
 				</div>
-
         	</div>
-   		
    	</section>
 
-   	</div>
+</div>
