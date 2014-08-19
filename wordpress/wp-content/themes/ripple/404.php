76c478c1bed43772ad716353bb0e8e30c61120a2
Ben Stein-Lobovits <ben@ripple.com>
2014-08-19 14:02:10 -0700

    [FIX] change 404 image

--- wordpress/wp-content/themes/ripple/404.php
+++ wordpress/wp-content/themes/ripple/404.php
@@ -16,7 +16,7 @@ get_header('targets'); ?>
 
 					<div class="col-sm-12 404">
 						<p><?php echo "<h3>Well, this is embarrasing. Check out our <a href=https://ripple.com/forum/>forums</a> or head back to <a href=https://ripple.com/>home</a>. Perhaps search Ripple below ?</h3>" ?></p>
-						<p><img src="https://ripple.com/wp-content/uploads/2014/08/404.png"></p>
+						<p><img src="https://ripple.com/wp-content/uploads/2014/08/404_2.png"></p>
 						<p>Search Ripple:</p>
 						<?php get_search_form(); ?>
 					</div>
